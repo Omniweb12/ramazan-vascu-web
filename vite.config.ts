aa9933aeb1f59128c3c0729a4b362e7358946aee
Omniweb12 <backupdesign@yahoo.com>
2025-07-31 16:35:26 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,17 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import ghPages from "vite-plugin-gh-pages";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/ramazan-vascu-web/", // foarte important pentru GitHub Pages
   server: {
     host: "::",
     port: 8080,
   },
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
+    ghPages(), // adăugăm pluginul de deploy
   ].filter(Boolean),
   resolve: {
     alias: {
