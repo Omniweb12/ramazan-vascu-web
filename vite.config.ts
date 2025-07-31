e5c9fd1467683783ee1c1f3b593ff38538841111
Omniweb12 <backupdesign@yahoo.com>
2025-07-31 16:57:22 +0300

    Add files via upload

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/ramazan-vascu-web/",  // AICI!
+  base: "/ramazan-vascu-web/",
   server: {
     host: "::",
     port: 8080,
